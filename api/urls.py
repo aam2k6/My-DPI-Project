79c17291d7c22dec8f3666dd0cace2912e4d321e
Hemant Kumar <hemantkumarcpersonal@gmail.com>
2024-07-26 22:03:50 +0530

    Change Connection Model, Add Create New Conxn

--- api/urls.py
+++ api/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [path('dpi-directory/', views.dpi_directory, name='dpi-directory')
                path('get-lockers-user/', views.get_lockers_user, name='get-lockers-user'),
                path('get-other-connection-types/', views.get_other_connection_types, name='get-other-connection-types'),
                path('connection_types/', views.get_connectiontype_by_user_by_locker, name='get_connection_types'),
-               path('create_new_connection/', views.create_new_connection, name='create_new_connection'),
+               path('create-new-connection/', views.create_new_connection, name='create_new_connection'),
                path('login-user/', views.login_view, name='login'),
                path('show_terms/', show_terms, name='show_terms'),
                path('give_consent', views.give_consent, name='give_consent'),

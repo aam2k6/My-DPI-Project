ac438419955e176ea5ddf00b34a78c8a2f4aed51
valmikGit <Valmik.Belgaonkar@iiitb.ac.in>
2024-08-07 16:30:04 +0530

    removed user_passes_test import in views.py.

--- api/views.py
+++ api/views.py
@@ -10,8 +10,7 @@ from rest_framework.authentication import BasicAuthentication
 from rest_framework.decorators import (
     api_view,
     permission_classes,
-    authentication_classes,
-    user_passes_test,
+    authentication_classes
 )
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response

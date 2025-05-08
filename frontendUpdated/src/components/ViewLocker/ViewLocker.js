b96ab2e0d1fa22087cbfb2e3bce6b01301d96048
sachin poojary <sachikrishna1997@gmail.com>
2025-05-08 09:27:57 +0530

    ...

--- frontendUpdated/src/components/ViewLocker/ViewLocker.js
+++ frontendUpdated/src/components/ViewLocker/ViewLocker.js
@@ -5,7 +5,7 @@ import Cookies from "js-cookie";
 import { useLocation, useParams } from "react-router-dom";
 import { usercontext } from "../../usercontext";
 import Navbar from "../Navbar/Navbar";
-import Sidebar from "../Sidebar/Sidebar";
+// import Sidebar from "../Sidebar/Sidebar";
 import { frontend_host } from "../../config";
 import { QrReader } from "react-qr-reader";
 import Modal from "../Modal/Modal";
@@ -1524,10 +1524,11 @@ export const ViewLocker = () => {
         className={`hamburger-menu ${isSidebarOpen ? "hidden" : ""}`}
         onClick={toggleSidebar}
       >
-        <Menu size={24} />
+        <h1>Menu</h1>
+        {/* <Menu size={24} /> */}
       </button>
 
-      <Sidebar
+      {/* <Sidebar
         isSidebarOpen={isSidebarOpen}
         toggleSidebar={toggleSidebar}
         activeMenu={activeMenu}
@@ -1536,7 +1537,7 @@ export const ViewLocker = () => {
         toggleSubmenu={toggleSubmenu}
         lockerObj={locker}
         locker_on={true}
-      />
+      /> */}
 
 <div className="locker-header">
         <div className="locker-text">
